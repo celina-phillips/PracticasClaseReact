456426be1fed75198beed9d03cfabfd1b7593c0f
celina-phillips <phillipscelina98@gmail.com>
2023-02-24 17:59:51 -0300

    primer comit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Titulo from "./components/Titulo";
+
+
+function App() {
+
+  let nombre = "celina"
+  return (
+    <>
+    <Titulo nombre={nombre}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Titulo = (props) => {
+return (
+    <>
+    <h1>Hello {props.nombre}</h1>
+    </>
+)
+}
+
+export default Titulo
